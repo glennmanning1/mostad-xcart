56cc44cda77471f8aaf72135d2f0c69f584d7f49
Drew Brown <drew.brown@novahorizons.io>
2016-03-15 15:46:47 -0500

    * Initial commit up mostad coupon modifiers

---
+++ classes/XLite/Module/Mostad/Coupons/Model/Coupon.php
@@ -0,0 +1,123 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Nova Horizons
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the software license agreement
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @category  X-Cart 5
+ * @author    Nova Horzions LLC <info@novahorizons.io>
+ * @copyright Copyright (c) 2015 Nova Horzions LLC <info@novahorizons.io>. All rights reserved
+ * @license   http://novahorizons.io/x-cart/license License Agreement
+ * @link      http://novahorizons.io/
+ */
+
+namespace XLite\Module\Mostad\Coupons\Model;
+
+
+use Doctrine\ORM\Mapping\JoinColumn;
+
+class Coupon extends \XLite\Module\CDev\Coupons\Model\Coupon implements \XLite\Base\IDecorator
+{
+
+    /**
+     * @var boolean
+     *
+     * @Column(name="free_shipping", type="boolean")
+     */
+    protected $freeShipping = false;
+
+    /**
+     * @var \XLite\Model\Product
+     *
+     * @ManyToOne(targetEntity="XLite\Model\Product")
+     * @JoinColumn(name="product_id", referencedColumnName="product_id", onDelete="CASCADE")
+     */
+    protected $product;
+
+    /**
+     * Coupon constructor.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data = array())
+    {
+        $this->products = new \Doctrine\Common\Collections\ArrayCollection();
+
+        parent::__construct($data);
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFreeShipping()
+    {
+        return static::TYPE_FREESHIP == $this->getType() ||
+               $this->freeShipping;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getFreeShipping()
+    {
+        return $this->freeShipping;
+    }
+
+    /**
+     * @param $freeShipping
+     *
+     * @return $this
+     */
+    public function setFreeShipping($freeShipping)
+    {
+        $this->freeShipping = $freeShipping;
+
+        return $this;
+    }
+
+    public function setProduct(\XLite\Model\Product $product)
+    {
+        if (!$product instanceof \XLite\Model\Product) {
+            $product = \XLite\Core\Database::getRepo('XLite\Model\Product')->find($product);
+        }
+
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function isValidForProduct(\XLite\Model\Product $product)
+    {
+        $result = true;
+
+        if ($this->getProduct() == $product) {
+            return $result;
+        }
+
+        if (0 < count($this->getProductClasses())) {
+            // Check product class
+            $result = $product->getProductClass()
+                && $this->getProductClasses()->contains($product->getProductClass());
+        }
+
+        if ($result && 0 < count($this->getCategories())) {
+            // Check categories
+            $result = false;
+            foreach ($product->getCategories() as $category) {
+                if ($this->getCategories()->contains($category)) {
+                    $result = true;
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+}
---
+++ classes/XLite/Module/Mostad/Coupons/View/Model/Coupon.php
@@ -0,0 +1,66 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Nova Horizons
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the software license agreement
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @category  X-Cart 5
+ * @author    Nova Horzions LLC <info@novahorizons.io>
+ * @copyright Copyright (c) 2015 Nova Horzions LLC <info@novahorizons.io>. All rights reserved
+ * @license   http://novahorizons.io/x-cart/license License Agreement
+ * @link      http://novahorizons.io/
+ */
+
+namespace XLite\Module\Mostad\Coupons\View\Model;
+
+/**
+ * Coupon model form extension
+ *
+ * @LC_Dependencies("CDev\Coupons","XC\FreeShipping")
+ */
+class Coupon extends \XLite\Module\CDev\Coupons\View\Model\Coupon implements \XLite\Base\IDecorator
+{
+
+    /**
+     * Coupon constructor.
+     *
+     * @param array $params
+     * @param array $sections
+     */
+    public function __construct(array $params = array(), array $sections = array())
+    {
+        $freeShippingField = array();
+        $freeShippingField['freeShipping'] = array(
+            self::SCHEMA_CLASS => 'XLite\Module\CDev\Coupons\View\FormField\Enabled',
+            self::SCHEMA_LABEL => 'Include Free Shipping',
+        );
+
+        $this->schemaDefault = array_slice($this->schemaDefault, 0, 5, true)
+            + $freeShippingField
+            + array_slice($this->schemaDefault, 5, count($this->schemaDefault)-5, true);
+        
+        $this->schemaDefault['product'] = array(
+            self::SCHEMA_CLASS => 'XLite\View\FormField\Select\Model\ProductSelector',
+            self::SCHEMA_LABEL => 'Product',
+        );
+
+        return parent::__construct($params, $sections);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getFormFieldsForSectionDefault()
+    {
+        parent::getFormFieldsForSectionDefault();
+
+        $this->schemaDefault['freeShipping'][self::SCHEMA_DEPENDENCY][self::DEPENDENCY_HIDE]['type'] = array(\XLite\Module\CDev\Coupons\Model\Coupon::TYPE_FREESHIP);
+
+        return $this->getFieldsBySchema($this->schemaDefault);
+    }
+}
