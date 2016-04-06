da8e1d80f843b9dcb0209906c75326b18bd73572
Drew Brown <drew.brown@novahorizons.io>
2016-04-06 15:57:48 -0500

    * Removed the product tabs.

---
+++ classes/XLite/Module/Mostad/CustomTheme/View/Product/Details/Customer/Page/APage.php
@@ -0,0 +1,28 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Nova Horizons
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the software license agreement
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @category  X-Cart 5
+ * @author    Nova Horzions LLC <info@novahorizons.io>
+ * @copyright Copyright (c) 2015 Nova Horzions LLC <info@novahorizons.io>. All rights reserved
+ * @license   http://novahorizons.io/x-cart/license License Agreement
+ * @link      http://novahorizons.io/
+ */
+
+namespace XLite\Module\Mostad\CustomTheme\View\Product\Details\Customer\Page;
+
+
+abstract class APage extends \XLite\View\Product\Details\Customer\Page\APage implements \XLite\Base\IDecorator
+{
+    protected function getTabs()
+    {
+        return array();
+    }
+}
