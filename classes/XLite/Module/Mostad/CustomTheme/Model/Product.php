e481cb7222a971e94006b275732db5b9ca34303e
Drew Brown <drew.brown@novahorizons.io>
2016-04-14 13:11:36 -0500

    * Updated gallery first load * Removed mouse wheel quantity box functionality

---
+++ classes/XLite/Module/Mostad/CustomTheme/Model/Product.php
@@ -0,0 +1,43 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Nova Horizons
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the software license agreement
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @category  X-Cart 5
+ * @author    Nova Horzions LLC <info@novahorizons.io>
+ * @copyright Copyright (c) 2015 Nova Horzions LLC <info@novahorizons.io>. All rights reserved
+ * @license   http://novahorizons.io/x-cart/license License Agreement
+ * @link      http://novahorizons.io/
+ */
+
+namespace XLite\Module\Mostad\CustomTheme\Model;
+
+/**
+ * @LC_Dependencies("XC\ProductVariants")
+ */
+class Product extends \XLite\Model\Product implements \XLite\Base\IDecorator
+{
+
+    /**
+     * Get public images
+     *
+     * @return array
+     */
+    public function getPublicImages()
+    {
+        $list = parent::getPublicImages();
+
+        if ($this->isUseVariantImage()) {
+            array_unshift($list, $this->getDefaultVariant()->getImage());
+        }
+
+        return $list;
+    }
+
+}
