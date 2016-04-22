2be648fd6327ce917fcecd40b95cd85284139d40
Drew Brown <drew.brown@novahorizons.io>
2016-04-22 13:08:21 -0500

    * Updated handling of minimum quantities

--- classes/XLite/Module/Mostad/CustomTheme/View/Product/QuantityBox.php
+++ classes/XLite/Module/Mostad/CustomTheme/View/Product/QuantityBox.php
@@ -18,7 +18,9 @@
 
 namespace XLite\Module\Mostad\CustomTheme\View\Product;
 
-
+/**
+ * @LC_Dependencies("QSL\AdvancedQuantity")
+ */
 class QuantityBox extends \XLite\View\Product\QuantityBox implements \XLite\Base\IDecorator
 {
     /**
@@ -37,6 +39,43 @@ class QuantityBox extends \XLite\View\Product\QuantityBox implements \XLite\Base
             . $this->getAdditionalValidate()
             . ']'
         );
+    }
+
+    /**
+     * Return minimum quantity
+     *
+     * @return integer
+     */
+    protected function getMinQuantity()
+    {
+        $minQuantity = $this->getProduct()->getMinQuantity(
+            $this->getCart()->getProfile() ? $this->getCart()->getProfile()->getMembership() : null
+        );
+
+        $result = parent::getMinQuantity();
+
+        $minimumQuantity = $minQuantity ? $minQuantity : $result;
+
+        if (!$this->isCartPage()) {
+
+            $items = \XLite\Model\Cart::getInstance()->getItemsByProductId($this->getProduct()->getProductId());
+
+            $quantityInCart = $items
+                ? \Includes\Utils\ArrayManager::sumObjectsArrayFieldValues(
+                    $items,
+                    'getAmount',
+                    true
+                )
+                : 0;
+
+            $result = ($minimumQuantity > $quantityInCart) ? ($minimumQuantity - $quantityInCart) : $result;
+
+        } else {
+
+            $result = $minimumQuantity;
+        }
+
+        return $result;
     }
 
 }
