4e25efaa5e95538bda35e90bb004657b7c4051c4
Phillip Brand <phil@novahorizons.io>
2016-05-31 13:54:26 -0500

    Disable imprint form fields if user selects 'use info on file'

--- skins/default/en/modules/Mostad/ImprintingInformation/address/controller.js
+++ skins/default/en/modules/Mostad/ImprintingInformation/address/controller.js
@@ -30,3 +30,20 @@ core.bind(
     jQuery('.model-not-defined', $wrapper).removeClass('hidden');
   }
 );
+
+
+$(function() {
+    if ($('.status-value input:checked').val() == 'SAME') {
+        $('.disable-for-same').attr('disabled', 'disabled');
+    }
+
+    $('.status-value input').change(function() {
+        var $formEls = $('.disable-for-same');
+        if (this.value == 'SAME') {
+            $formEls.attr('disabled', 'disabled');
+        } else {
+            $formEls.removeAttr('disabled');
+        }
+    });
+
+});
